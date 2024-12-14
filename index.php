780ffb2ac886f8c2d35bdd9502b3906817d14051
acidnine <acidnine@users.noreply.github.com>
2024-12-13 22:23:18 -0700

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,14 @@ require_once 'app/auth.php';
     <script src="js/checkName.js"></script>
     <nav>
         <a href="index.php"><img alt="PiVPN Logo" src="img/pivpnlogo64.png" width="32"></a>
-        <h1>PiVPN <span class="version">(OpenVPN v<?php echo shell_exec("openvpn --version | awk '/OpenVPN/ {print $2\")\"}' | head -n 1") ?></span></h1>
+        <?php
+        $install_check = shell_exec("openvpn --version");
+        if($install_check){
+        ?>
+        <h1>PiVPN <span class="version">(OpenVPN v<?=shell_exec("openvpn --version | awk '/OpenVPN/ {print $2\")\"}' | head -n 1")?></span></h1>
+        <?php }else{ ?>
+        <h1>PiVPN <span class="version">(Wireguard <?=shell_exec("wg --version | awk '/ v/ {print $2\")\"}'")?></span></h1>
+        <?php } ?>
         <button class="reload" onClick="window.location.reload();">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                 <path d="M13.5 2c-5.621 0-10.211 4.443-10.475 10h-3.025l5 6.625 5-6.625h-2.975c.257-3.351 3.06-6 6.475-6 3.584 0 6.5 2.916 6.5 6.5s-2.916 6.5-6.5 6.5c-1.863 0-3.542-.793-4.728-2.053l-2.427 3.216c1.877 1.754 4.389 2.837 7.155 2.837 5.79 0 10.5-4.71 10.5-10.5s-4.71-10.5-10.5-10.5z"/>
