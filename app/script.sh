53854a120604940d609b3591b01d81a26310d8b6
acidnine <acidnine@users.noreply.github.com>
2024-12-13 21:24:48 -0700

    Update script.sh
    This resolves the inability to login as previously it only allowed users who setup openvpn to login, now pivpn configured wireguard users can login.

--- app/script.sh
+++ app/script.sh
@@ -2,7 +2,12 @@
 
 user=$1
 password=$2
-userPivpn=$(sudo cat /etc/pivpn/openvpn/setupVars.conf | grep install_user | sed s/install_user=//)
+config_file="/etc/pivpn/openvpn/setupVars.conf"
+if [ -f "$config_file" ]; then
+    userPivpn=$(sudo cat /etc/pivpn/openvpn/setupVars.conf | grep install_user | sed s/install_user=//)
+else
+    userPivpn=$(sudo cat /etc/pivpn/wireguard/setupVars.conf | grep install_user | sed s/install_user=//)
+fi
 
 if echo "$password" | su -c "echo" "$user" >/dev/null 2>&1 && [ "$user" == "$userPivpn" ]; then
     echo "Authenticated"
