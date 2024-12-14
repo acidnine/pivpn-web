a7227e08658af5c3c5e73a5c20b4caf5365401d2
acidnine <acidnine@users.noreply.github.com>
2024-12-13 21:31:32 -0700

    Update disable.php

--- app/disable.php
+++ app/disable.php
@@ -5,7 +5,12 @@ require_once 'auth.php';
 <?php
 $file = $_GET['file'];
 
-$command = "ip=\$(sudo sed -n 's/ifconfig-push \(.*\) .*/\\1/p' /etc/openvpn/ccd/".$file.") && sudo sed -i \"1s/.*/ifconfig-push 0.0.0.1 255.255.255.0/\" /etc/openvpn/ccd/".$file." && sudo sed -i \"\$ a\\#\$ip\" /etc/openvpn/ccd/".$file;
+config_file="/etc/pivpn/openvpn/setupVars.conf"
+if [ -f "$config_file" ]; then
+    $command = "ip=\$(sudo sed -n 's/ifconfig-push \(.*\) .*/\\1/p' /etc/openvpn/ccd/".$file.") && sudo sed -i \"1s/.*/ifconfig-push 0.0.0.1 255.255.255.0/\" /etc/openvpn/ccd/".$file." && sudo sed -i \"\$ a\\#\$ip\" /etc/openvpn/ccd/".$file;
+else
+    $command = "ip=\$(sudo sed -n 's/ifconfig-push \(.*\) .*/\\1/p' /etc/wireguard/".$file.") && sudo sed -i \"1s/.*/ifconfig-push 0.0.0.1 255.255.255.0/\" /etc/wireguard/".$file." && sudo sed -i \"\$ a\\#\$ip\" /etc/wireguard/".$file;
+fi
 
 $output = shell_exec($command);
 
@@ -16,10 +21,11 @@ if ($output == null) {
     header("Refresh:0, URL=$currentPath/../index.php");
 } else {
     $logFilePath = '../logs/error.log';
-    $errorMessage = 'Error disabling user: Check if the file '.$file.' exists in the directory "/etc/openvpn/ccd/" or if it has the necessary permissions.';
-
+    $username = exec('whoami');
+    $errorMessage = 'Error disabling user: Check if the file '.$file.' exists in the directory openvpn: "/etc/openvpn/ccd/" or wireguard: "/home/'.$username.'/configs/" or if it has the necessary permissions.';
+    
     file_put_contents($logFilePath, date('Y-m-d H:i:s') . ' - ' . $errorMessage . "\n", FILE_APPEND);
-
+    
     echo $errorMessage;
 }
 
